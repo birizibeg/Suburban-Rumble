37f1e0b3d8695e9f5d7046a2c7f4e9b359daaebc
Boaz Joseph <bsj8@pitt.edu>
2022-10-11 23:41:16 -0400

    fixed crash

--- code/src/conversation.rs
+++ code/src/conversation.rs
@@ -52,19 +52,20 @@ pub fn setup_conversation(
 	}).insert(Enemy);
 	let box_size = Vec2::new(300.0, 200.0);
     let box_position = Vec2::new(0.0, -250.0);
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::DARK_GRAY,
-            custom_size: Some(Vec2::new(box_size.x, box_size.y)),
-            ..default()
-        },
-        transform: Transform::from_translation(box_position.extend(0.0)),
-        ..default()
-    });
+    // commands.spawn_bundle(SpriteBundle {
+    //     sprite: Sprite {
+    //         color: Color::DARK_GRAY,
+    //         custom_size: Some(Vec2::new(box_size.x, box_size.y)),
+    //         ..default()
+    //     },
+    //     transform: Transform::from_translation(box_position.extend(0.0)),
+    //     ..default()
+    // });
     commands.spawn_bundle(Text2dBundle {
-        text: Text::from_section("Dialogue Box", text_style),
+        text: Text::from_section("Press enter to display input", text_style),
         text_2d_bounds: Text2dBounds {
             size: box_size,
+
         },
         transform: Transform::from_xyz(
             box_position.x - box_size.x / 2.0,
@@ -80,14 +81,19 @@ pub fn clear_conversation(
     mut clear_color: ResMut<ClearColor>,
     mut hero: Query<Entity, With<Hero>>,
 	mut enemy: Query<Entity, With<Enemy>>,
+    mut dialogue:Query<Entity,With<DialogueBox>>,
 
 ) {
     clear_color.0 = Color::BLACK;
 
     let hero_eid = hero.single_mut();
 	let enemy_eid = enemy.single_mut();
+    let dialogue_eid:Entity = dialogue.single_mut();
     commands.entity(hero_eid).despawn();
 	commands.entity(enemy_eid).despawn();
+    commands.entity(dialogue_eid).despawn();
+    
+
 
 }
 
